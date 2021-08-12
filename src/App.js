7b5cf381cdd3b3298be02150bfe83fc7bb06268d
Musa Joshua <gidijosh@gmail.com>
2021-08-12 07:18:03 +0100

    feat: task 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import 'rsuite/dist/styles/rsuite-dark.css';
 
 import { Counter } from './features/counter/Counter';
 
-import CustomDropdown from './components/CustomDropDown';
 import CustomForm from './components/CustomForm';
+import CustomForm2 from './components/CustomForm2';
+import CustomDropdown from './components/CustomDropDown';
+import DisplayForm from './components/DisplayForm';
 
 import Home from './components/Home';
 import Profile from './components/Profile';
@@ -30,10 +32,45 @@ const menuOptions = {
 }
 
 function App() {
-  const [Component, setComponent] = useState(null)
+  const [SelectedComponent, setSelectedComponent] = useState(null)
+  const [data, setData] = useState([])
+  const [formData, setFormData] = useState({
+    punchline: '',
+    setup:'',
+    type:''
+  });
+
   const selectOnChange = (value) => {
-    setComponent(stringToComponent[value])
+    setSelectedComponent(stringToComponent[value])
   }
+
+  const inputOnChange = (value, event) => {
+    const { name } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault();
+
+    setData((prev) => ([
+      formData,
+      ...prev
+    ]))
+  }
+
+  console.log(data)
+
+  useEffect(() => {
+    const fetchData  = async () => {
+      const res = await fetch(process.env.REACT_APP_API_URL);
+      const result = await res.json()
+      setData(result)
+    }
+    fetchData()
+  }, [])
 
   return (
     <div>
@@ -48,7 +85,12 @@ function App() {
       <div>
         <h1>Task 3:</h1>
         <CustomForm selectOnChange={selectOnChange} />
-        {Component && Component}
+        {SelectedComponent && SelectedComponent }
+      </div>
+      <div>
+        <h1>Task 4:</h1>
+        <CustomForm2 formData={formData} inputOnChange={inputOnChange} submitForm={submitForm} />
+        <DisplayForm data={data} />
       </div>
     </div>
   );
