f7161a6790090c9d9d90cd50eb15bf34909ad42c
Musa Joshua <gidijosh@gmail.com>
2021-08-12 06:08:56 +0100

    feat: install rsuite

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import 'rsuite/dist/styles/rsuite-dark.css';
+
 import { Counter } from './features/counter/Counter';
-import './App.css';
+
+import CustomDropdown from './components/CustomDropDown';
+import CustomForm from './components/CustomForm';
+
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+const stringToComponent  = {
+  Home,
+  Profile
+}
+const menuOptions = {
+  title: 'Menu',
+  options: [
+    {
+      title: 'Home',
+    },
+    {
+      title: 'About us',
+    },
+    {
+      title: 'Contact US',
+    }
+  ]
+}
 
 function App() {
+  const [Component, setComponent] = useState(null)
+  const selectOnChange = (value) => {
+    setComponent(stringToComponent[value])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div>
+        <h1>Task 1:</h1>
         <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      </div>
+      <div>
+        <h1>Task 2:</h1>
+        <CustomDropdown menuOptions={menuOptions} />
+      </div>
+      <div>
+        <h1>Task 3:</h1>
+        <CustomForm selectOnChange={selectOnChange} />
+        {Component && Component}
+      </div>
     </div>
   );
 }
